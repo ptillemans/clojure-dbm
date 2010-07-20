011133e95464d004bacc8c0def22ba5b23af8f30
Peter Tillemans <pti@melexis.com>
2010-07-21 00:19:10 +0200

    Rudimentary leiningen support

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure-dbm "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
+		 [org.clojars.db/tokyo-cabinet "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     ] )
